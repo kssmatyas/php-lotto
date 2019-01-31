472558de60bc6b8f8bbbb7ba19803f26304d7b8f
diak <diak@10.9.8.8>
2019-01-31 08:06:58 +0100

    lotto ki ir szamokat

--- index.php
+++ index.php
@@ -9,6 +9,18 @@
     <body>
         Ez az index.php
         <?php
+            //lottohuzas
+            $kihuzottak=0;
+            while($kihuzottak<5)
+            {
+                $sorsoltszam=rand(1,90);
+                if(!isset($tomb[$sorsoltszam]))
+                {
+                    $tomb[$sorsoltszam]=1;
+                    $kihuzottak++;
+                }
+            }
+            var_dump($tomb);
         echo "itt már a php script fut";
         ?>
     </body>
