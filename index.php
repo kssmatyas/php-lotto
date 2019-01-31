548980f9d998c05b030440a3e561f3db207e42ce
diak <diak@10.9.8.8>
2019-01-31 07:54:54 +0100

    Alapváltozat

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        Ez az index.php
+        <?php
+        
+        
+        echo "jeeeeee";
+        ?>
+    </body>
+</html>
